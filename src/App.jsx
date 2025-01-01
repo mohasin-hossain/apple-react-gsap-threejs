fbbd4e18a5aeadbc84797114319dc548158f302b
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-01-01 17:12:03 +0100

    Integrated Sentry

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
+import * as Sentry from "@sentry/react";
+
 const App = () => {
+
+
   return (
     <main>
       <Navbar />
@@ -14,4 +18,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,28 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://ea0338e8ce7b1f776f7b6f79204caa42@o4508569168904192.ingest.de.sentry.io/4508569171263568",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
